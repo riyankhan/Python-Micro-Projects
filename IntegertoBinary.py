4ace920c0c86a4c299412e5caaea83d8a7ecf6b6
Mohd Riyan Khan <34193886+bytesandcodes@users.noreply.github.com>
2018-03-05 13:33:45 +0530

    Add files via upload

---
+++ IntegertoBinary.py
@@ -0,0 +1,13 @@
+
+
+#Following program converts decimal numbers to its binary form
+#It only takes input as an integer and not floating value
+
+num = int(input("Enter an integer: "))
+decimal_num = num
+binary_bits=[]
+while num>0:    
+    num, remainder = divmod(num, 2)
+    binary_bits.append(remainder)
+binary_bits= binary_bits[::-1]
+print("The Binary coversion of ", decimal_num, " is: ", *binary_bits, sep='')
