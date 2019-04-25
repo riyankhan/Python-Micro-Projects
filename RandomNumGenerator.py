4619e5cc2d5885737f375e1d95918f801eddae5a
Mohd Riyan Khan <34193886+bytesandcode@users.noreply.github.com>
2019-04-25 19:35:09 +0530

    Update RandomNumGenerator.py

--- RandomNumGenerator.py
+++ RandomNumGenerator.py
@@ -1,7 +1,7 @@
 #This program is optimized for Python 3
 #It should work on Python 2 as well but with some modifications
 #You can use this program to generate a random number or you can also use this program as a dice.
-#For more such interesting Python programs, visit: https://github.com/bytesandcodes
+#For more such interesting Python programs, visit: https://github.com/bytesandcode
 
 from random import randint
 user_answer = input("Want to generate a random number? Enter y for yes and n for no.: ").lower().strip()
